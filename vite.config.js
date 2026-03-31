7d2697817489b4889fdb536f9cb399187c7e9ce7
zwivhuya ratshivhanda <ratshivhanda72@gmail.com>
2026-03-31 04:03:11 -0400

    adding the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
-  base: "/zwivhu97",
+  base: "/e-plantShopping/",
   plugins: [react()],
 })
